4eddd8a5d92d6ec6ace114bc10bae80ccc69fdf9
Raul-madero <r.madero.ramirez@gmail.com>
2023-05-04 20:05:07 -0600

    terminando el aside

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,25 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.html.js"],
   theme: {
+    colors: {
+      legoyellow: '#ffd602',
+      legored: '#d5040a',
+      legoblue: '#036cb6',
+      white: '#fff',
+    },
+    fontFamily: {
+      display: 'Lato, Arial, sans-serif'
+    },
     extend: {
       animation: {
         loader: 'loader 2s linear forwards',
         scaling: 'scaling 2s ease infinite',
         scaling1: 'scaling1 2s ease infinite',
         scaling2: 'scaling2 2s ease infinite',
+        navbar: 'navbar 2s 0.5s ease-in-out',
+        maincontent: 'maincontent 2s 0.5s ease-in-out',
+        super: 'super 0.5s 2s ease-in-out forwards',
+        superman: 'superman 1.5s 1.5s ease-in-out forwards'
       },
       keyframes: {
         loader: {
@@ -42,6 +55,41 @@ module.exports = {
           },
           '60%, 70%': {
             transform: 'scale(1)'
+          }
+        },
+        navbar: {
+          '0%': {
+            transform: 'translateY(-200px)'
+          },
+          '100%': {
+            transform: 'translateY(0)'
+          }
+        },
+        maincontent: {
+          '0%': {
+            transform: 'translateY(-1000px)'
+          },
+          '100%': {
+            transform: 'translateY(0)'
+          }
+        },
+        super: {
+          '0%' : {
+            opacity: 0
+          },
+          '100%' :{
+            opacity: 1
+          }
+        },
+        superman: {
+          '0%' : {
+            transform: 'translateX(-1000px)'
+          },
+          '50%': {
+            transform: 'translateX(150px)'
+          },
+          '100%': {
+            transform: 'translateX(0px)'
           }
         }
       }
